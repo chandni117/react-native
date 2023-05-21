ba265b2bdebc87e5070ae91f6fc7e8c44887a95b
chandni117 <chandnirai.cs@gmail.com>
2023-05-21 02:10:04 -0700

    Adding Tab navigator

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import { StyleSheet,  View , StatusBar} from 'react-native';
-import LemonHeader from './components/LemonHeader';
-import LemonFooter from './components/LemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
 import MenuItems from './components/MenuItems';
-import FeedbackScreen from './components/feedbackScreen';
-import LoginPage from './components/LoginPage';
 import Welcome from './components/Welcome';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import {Ionicons} from '@expo/vector-icons' ;
 
-const stack = createNativeStackNavigator() ;
+
+const tab = createBottomTabNavigator() ;
 export default function App() {
   return (
   <>
       <NavigationContainer>
         <View style={styles.container}>
-        <LemonHeader />
-        <stack.Navigator 
+        <tab.Navigator 
           initialRouteName='Login Page'
-          screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' }}}>
-          <stack.Screen 
+          screenOptions={({route}) => ({
+            tabBarIcon : ({focused , color, size }) => {
+              let iconName ;
+
+              if(route.name === 'Welcome')
+              {
+                iconName : focused ? 'information-circle':
+                'information-circle-outline';
+              }
+              else if(route.name=== 'Menu')
+              {
+                iconName: 'list-circle' ; 
+              }
+              return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor: 'gray' ,
+            tabBarInactiveTintColor: 'orange'
+          })}>
+          <tab.Screen 
             name="Welcome" 
-            component={Welcome} 
-            options={{ title: 'Home' }} ></stack.Screen>
-          <stack.Screen name="Menu" component={MenuItems} ></stack.Screen>
-        </stack.Navigator>
-        </View>
-        <View style={styles.footerStyle}> 
-          <LemonFooter />
+            component={Welcome}  ></tab.Screen>
+          <tab.Screen name="Menu" component={MenuItems} ></tab.Screen>
+        </tab.Navigator>
         </View>
       </NavigationContainer>
     </> 
@@ -38,10 +47,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#333333'
-  },
-  footerStyle: {
-    backgroundColor: '#333333'
   }
+
  
 })
 
